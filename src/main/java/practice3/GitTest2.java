a94bd56cd05573115d9eb716c0a2bb4d05526e2f
Nishat1987 <nishat616901@gmail.com>
2022-12-17 14:16:29 -0600

    3rd commit

---
+++ src/main/java/practice3/GitTest2.java
@@ -0,0 +1,23 @@
+package practice3;
+
+public class GitTest2 {
+
+	public static void main(String[] args) {
+	printme();
+	printme2();
+
+	}
+
+	public static void printme() {
+		System.out.println("Git test");
+	}
+
+	public static void printme2() {
+		System.out.println("Git test 2");
+		System.out.println("Git test 3");
+		System.out.println("Git test 4");
+		System.out.println("Git test 5");
+		System.out.println("Git test 6");
+	}
+
+}
